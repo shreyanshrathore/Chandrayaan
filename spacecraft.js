c970dd3e8462898f754be956fad0411cd3a00328
shreyansh-error-89 <shreyanshrathore75649@gmail.com>
2023-08-05 22:18:22 +0530

    Starting Template

---
+++ spacecraft.js
@@ -0,0 +1,35 @@
+// spacecraft.js
+
+class Spacecraft {
+    constructor() {
+      this.position = { x: 0, y: 0, z: 0 };
+      this.direction = 'N';
+    }
+  
+    moveForward() {
+      // Implement movement logic
+    }
+  
+    moveBackward() {
+      // Implement movement logic
+    }
+  
+    turnLeft() {
+      // Implement rotation logic
+    }
+  
+    turnRight() {
+      // Implement rotation logic
+    }
+  
+    turnUp() {
+      // Implement angle change logic
+    }
+  
+    turnDown() {
+      // Implement angle change logic
+    }
+  }
+  
+  module.exports = Spacecraft;
+  
---
+++ spacecraft.test.js
@@ -0,0 +1,35 @@
+// spacecraft.test.js
+
+const Spacecraft = require('./spacecraft');
+
+describe('Spacecraft', () => {
+  let spacecraft;
+
+  beforeEach(() => {
+    spacecraft = new Spacecraft();
+  });
+
+  it('should move spacecraft forward', () => {
+    // Implement test case
+  });
+
+  it('should move spacecraft backward', () => {
+    // Implement test case
+  });
+
+  it('should turn spacecraft left', () => {
+    // Implement test case
+  });
+
+  it('should turn spacecraft right', () => {
+    // Implement test case
+  });
+
+  it('should turn spacecraft up', () => {
+    // Implement test case
+  });
+
+  it('should turn spacecraft down', () => {
+    // Implement test case
+  });
+});
