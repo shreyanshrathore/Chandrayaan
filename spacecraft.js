56ea1c4bb8fc68ac283e30bda32613412e13c7de
shreyansh-error-89 <shreyanshrathore75649@gmail.com>
2023-08-06 01:36:21 +0530

    First Attempt

--- spacecraft.js
+++ spacecraft.js
@@ -1,35 +1,154 @@
-// spacecraft.js
-
 class Spacecraft {
-    constructor() {
-      this.position = { x: 0, y: 0, z: 0 };
-      this.direction = 'N';
-    }
-  
-    moveForward() {
-      // Implement movement logic
-    }
-  
-    moveBackward() {
-      // Implement movement logic
-    }
-  
-    turnLeft() {
-      // Implement rotation logic
-    }
-  
-    turnRight() {
-      // Implement rotation logic
-    }
-  
-    turnUp() {
-      // Implement angle change logic
-    }
-  
-    turnDown() {
-      // Implement angle change logic
+  constructor(x, y, z, direction) {
+    this.position = { x, y, z };
+    this.direction = direction;
+  }
+
+  moveForward() {
+    switch (this.direction) {
+      case "N":
+        this.position.y += 1;
+        break;
+      case "S":
+        this.position.y -= 1;
+        break;
+      case "E":
+        this.position.x += 1;
+        break;
+      case "W":
+        this.position.x -= 1;
+        break;
+      case "Up":
+        this.position.z += 1;
+        break;
+      case "Down":
+        this.position.z -= 1;
+        break;
     }
   }
-  
-  module.exports = Spacecraft;
-  
+
+  moveBackward() {
+    switch (this.direction) {
+      case "N":
+        this.position.y -= 1;
+        break;
+      case "S":
+        this.position.y += 1;
+        break;
+      case "E":
+        this.position.x -= 1;
+        break;
+      case "W":
+        this.position.x += 1;
+        break;
+      case "Up":
+        this.position.z -= 1;
+        break;
+      case "Down":
+        this.position.z += 1;
+        break;
+    }
+  }
+
+  turnLeft() {
+    switch (this.direction) {
+      case "N":
+        this.direction = "W";
+        break;
+      case "S":
+        this.direction = "E";
+        break;
+      case "E":
+        this.direction = "N";
+        break;
+      case "W":
+        this.direction = "S";
+        break;
+      case "Up":
+        this.direction = "W";
+        break;
+      case "Down":
+        this.direction = "E";
+        break;
+    }
+  }
+
+  turnRight() {
+    switch (this.direction) {
+      case "N":
+        this.direction = "E";
+        break;
+      case "S":
+        this.direction = "W";
+        break;
+      case "E":
+        this.direction = "S";
+        break;
+      case "W":
+        this.direction = "N";
+        break;
+      case "Up":
+        this.direction = "E";
+        break;
+      case "Down":
+        this.direction = "W";
+        break;
+    }
+  }
+
+  turnUp() {
+    switch (this.direction) {
+      case "N":
+      case "S":
+      case "E":
+      case "W":
+        this.direction = "Up";
+        break;
+    }
+  }
+
+  turnDown() {
+    switch (this.direction) {
+      case "N":
+      case "S":
+      case "E":
+      case "W":
+        this.direction = "Down";
+        break;
+    }
+  }
+}
+
+module.exports = Spacecraft;
+
+// Initialize spacecraft
+const chandrayaan3 = new Spacecraft(0, 0, 0, "N");
+
+// Execute commands
+const commands = ["f", "r", "u", "b", "l"];
+for (const command of commands) {
+  switch (command) {
+    case "f":
+      chandrayaan3.moveForward();
+      break;
+    case "b":
+      chandrayaan3.moveBackward();
+      break;
+    case "r":
+      chandrayaan3.turnRight();
+      break;
+    case "l":
+      chandrayaan3.turnLeft();
+      break;
+    case "u":
+      chandrayaan3.turnUp();
+      break;
+    case "d":
+      chandrayaan3.turnDown();
+      break;
+  }
+}
+
+// Print final position and direction
+console.log("Final Position:", chandrayaan3.position);
+console.log("Final Direction:", chandrayaan3.direction);
--- spacecraft.test.js
+++ spacecraft.test.js
@@ -1,35 +1,41 @@
-// spacecraft.test.js
-
-const Spacecraft = require('./spacecraft');
+const Spacecraft = require('./Spacecraft');
 
 describe('Spacecraft', () => {
-  let spacecraft;
-
-  beforeEach(() => {
-    spacecraft = new Spacecraft();
+  it('should move forward correctly', () => {
+    const spacecraft = new Spacecraft(0, 0, 0, 'N');
+    spacecraft.moveForward();
+    expect(spacecraft.position).toEqual({ x: 0, y: 1, z: 0 });
   });
 
-  it('should move spacecraft forward', () => {
-    // Implement test case
+  it('should move backward correctly', () => {
+    const spacecraft = new Spacecraft(0, 0, 0, 'N');
+    spacecraft.moveBackward();
+    expect(spacecraft.position).toEqual({ x: 0, y: -1, z: 0 });
   });
 
-  it('should move spacecraft backward', () => {
-    // Implement test case
+  it('should turn left correctly', () => {
+    const spacecraft = new Spacecraft(0, 0, 0, 'N');
+    spacecraft.turnLeft();
+    expect(spacecraft.direction).toBe('W');
   });
 
-  it('should turn spacecraft left', () => {
-    // Implement test case
+  it('should turn right correctly', () => {
+    const spacecraft = new Spacecraft(0, 0, 0, 'N');
+    spacecraft.turnRight();
+    expect(spacecraft.direction).toBe('E');
   });
 
-  it('should turn spacecraft right', () => {
-    // Implement test case
+  it('should turn up correctly', () => {
+    const spacecraft = new Spacecraft(0, 0, 0, 'N');
+    spacecraft.turnUp();
+    expect(spacecraft.direction).toBe('Up');
   });
 
-  it('should turn spacecraft up', () => {
-    // Implement test case
+  it('should turn down correctly', () => {
+    const spacecraft = new Spacecraft(0, 0, 0, 'N');
+    spacecraft.turnDown();
+    expect(spacecraft.direction).toBe('Down');
   });
 
-  it('should turn spacecraft down', () => {
-    // Implement test case
-  });
+ 
 });
